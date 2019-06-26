9a05e88e7ac7bc0bf88c0587cd5d6ec511811f65
smkmth <dandavidnoon@gmail.com>
2019-06-26 15:33:13 +0100

    updated notes ui

--- Assets/Scripts/InGameTextReader.cs
+++ Assets/Scripts/InGameTextReader.cs
@@ -10,6 +10,7 @@ public class InGameTextReader : MonoBehaviour
     public GameObject ReaderUI;
     public TextMeshProUGUI displayText;
     public FirstPersonCharacterController controller;
+    public SmoothMouseLook mouseLook;
     public Button returnButton;
 
     public void Start()
@@ -25,16 +26,20 @@ public class InGameTextReader : MonoBehaviour
         displayText.text = text.TextToDisplay;
         controller.characterIsActive = false;
         Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        mouseLook.cameraControl = false;
 
 
     }
 
     public void ResumeGame()
     {
+        Time.timeScale = 1;
         ReaderUI.SetActive(false);
         displayText.text = "";
         controller.characterIsActive = true;
-        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.Locked;
+        mouseLook.cameraControl = true;
 
     }
     
