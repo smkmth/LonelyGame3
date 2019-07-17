0935227fde8ea6e75403f9a8db7886f8e3aadcfd
SmkMth <dandavidnoon@gmail.com>
2019-07-17 08:28:00 +0100

    settings menu

---
+++ Assets/Scripts/World/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    public GameObject mainMenuObject;
+    public GameObject settingsMenuObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void BackToMainMenu()
+    {
+        mainMenuObject.SetActive(true);
+        settingsMenuObject.SetActive(false);
+    }
+
+}
