6690644b53a1982476c80dc1232f7a2a78c19808
smkmth <dandavidnoon@gmail.com>
2019-08-07 12:51:34 +0100

    game over

--- Assets/Scripts/World/GameReset.cs
+++ Assets/Scripts/World/GameReset.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class GameReset : MonoBehaviour
 {
     public PlayerManager playerManager;
-    public GameObject gameOverScreen;
-
-
+    public GameObject loadingScreen;
+    public string firstScene;
+    public string secondScene;
+    public string thirdScene;
+    public string playerScene;
+ 
     public void ResetLevel()
     {
         Time.timeScale = 1.0f;
-        gameOverScreen.SetActive(false);
-        playerManager.ChangePlayerState(PlayerState.freeMovement);
-        SceneManager.LoadScene("House");
+        StartCoroutine(LoadNewScene());
+
+    }
+
+    IEnumerator LoadNewScene()
+    {
+        loadingScreen.SetActive(true);
+        yield return new WaitForEndOfFrame();
+        AsyncOperation async = SceneManager.LoadSceneAsync(firstScene, LoadSceneMode.Additive);
+        async = SceneManager.LoadSceneAsync(secondScene, LoadSceneMode.Additive);
+        async = SceneManager.LoadSceneAsync(thirdScene, LoadSceneMode.Additive);
+        async = SceneManager.LoadSceneAsync(playerScene, LoadSceneMode.Additive);
+
+        while (!async.isDone)
+        {
+            yield return null;
+        }
+
+        //after scene is loaded - wait for 2 seconds for the player to fall a bit - and everything to kind of shuffle in 
+        yield return new WaitForSeconds(0.1f);
+        loadingScreen.SetActive(false);
+
+
     }
 }
