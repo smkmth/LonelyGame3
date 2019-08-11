eccf0cf19905996d6979f72e70cb8e5245b7b680
SmkMth <dandavidnoon@gmail.com>
2019-08-11 19:31:57 +0100

    scene saved

--- Assets/Scripts/Events/GameEventTrigger.cs
+++ Assets/Scripts/Events/GameEventTrigger.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-
+/// <summary>
+/// a non specific game event trigger, 
+/// always fires when the player does the right action - has only one
+/// condition
+/// (the smart way to do this was to make player location a condition
+/// in the conditional event trigger, but that would mean it was always
+/// firing/checking)
+/// </summary>
 [AddComponentMenu("GameEvents/Game Event Trigger")]
 public class GameEventTrigger : AbstractGameEventTrigger 
 {
-
+    
     private GameEventReceiver[] eventReceivers;
 
     public override void Start()
