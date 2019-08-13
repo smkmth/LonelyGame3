de68c5a3c8f28f76962e76ecef649d028232fcf4
smkmth <dandavidnoon@gmail.com>
2019-08-13 16:06:32 +0100

    crank 1 working

--- Assets/Scripts/Events/AnimationEvent.cs
+++ Assets/Scripts/Events/AnimationEvent.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
+public enum AnimationEventType
+{
+    Trigger,
+    BoolTrue,
+    BoolFalse
+}
 /// <summary>
 /// an animation event calls a parameter on an animator when 
 /// the event is triggered
@@ -10,12 +15,24 @@ using UnityEngine;
 [AddComponentMenu("GameEvents/Animation Event")]
 public class AnimationEvent : GameEventReceiver
 {
-    
+    public AnimationEventType thisEventType;
+
     public string parameterToTrigger;
     public Animator animatorToActivate;
 
     public override void DoEvent()
     {
-        animatorToActivate.SetTrigger(parameterToTrigger);
+        switch (thisEventType)
+        {
+            case AnimationEventType.Trigger:
+                animatorToActivate.SetTrigger(parameterToTrigger);
+                break;
+            case AnimationEventType.BoolTrue:
+                animatorToActivate.SetBool(parameterToTrigger, true);
+                break;
+            case AnimationEventType.BoolFalse:
+                animatorToActivate.SetBool(parameterToTrigger, false);
+                break;
+        }
     }
 }
